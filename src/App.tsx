6638f6aad7a89dde5d44dd2214ee489732f73ef0
MeetAlica <feher.ali.96@gmail.com>
2024-06-19 00:10:44 +0200

    Created Login component and added to the routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import UserList from './components/UserList';
-//import Login from './components/Login';
+import Login from './components/Login';
 
 const App: React.FC = () => {
   return (
@@ -11,7 +11,8 @@ const App: React.FC = () => {
         <header className="App-header">
           <h1>Welcome to the Test App</h1>
           <Routes>
-            <Route path="/" element={<UserList />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/UserList" element={<UserList />} />
           </Routes>
         </header>
       </div>
---
+++ src/components/Login.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (username === 'admin' && password === 'password') {
+      toast.success('Login was successful!');
+      navigate('/UserList');
+    } else {
+      toast.error('Wrong username or password!');
+    }
+  };
+
+  return (
+    <div className="loginContainer">
+      <h2>Log in to your account</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            <input
+              placeholder="Username"
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              className="inputStyle"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              placeholder="Password"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              className="inputStyle"
+            />
+          </label>
+        </div>
+        <button type="submit" className="loginButton">
+          Submit
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
